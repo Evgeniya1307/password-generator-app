e44457437eb841a10b8d4b228cdfec4f08fc348e
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-05-09 22:40:06 +0400

    логика input

---
+++ src/ui/input/input.js
@@ -0,0 +1,25 @@
+import React from "react";
+import cx from "clsx";
+import styles from "./input.module.css";
+
+function Input({
+  type,
+  placeholder,
+  defaultValue,
+  disabled,
+  className,
+  readOnly,
+}) {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      defaultValue={defaultValue}
+      disabled={disabled}
+      className={cx(styles["input"], className)}
+      readOnly={readOnly}
+    ></input>
+  );
+}
+
+export default Input;
